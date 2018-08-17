2ec0bb5125feb2d336d0582277d637043c4bf103
Kamila de fatima santos oliveira <32311268+Kamilahsantos@users.noreply.github.com>
2018-08-17 09:04:47 -0300

    aula if

---
+++ IfTest.java
@@ -0,0 +1,13 @@
+package test;
+
+public class IfTest {
+
+    public static void main(String[] args) {
+        //If Example
+        int x = 10;
+
+        if (x < 20) {
+            System.out.print("X less than 20\n");
+        }
+    }
+}
