19e90747fcb10931ea31bd3ad5a699688d0bd54a
Kamila de fatima santos oliveira <32311268+Kamilahsantos@users.noreply.github.com>
2018-08-20 16:01:13 -0300

    for loop exercise

---
+++ ForLoopTestV2.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Scanner;
+
+public class ForLoopTestV2 {
+
+    public static void main(String[] args) {
+        System.out.println("Enter the number of elements to iterate:");
+        int maxElements;
+        Scanner scanner = new Scanner(System.in); //Creation of the Scanner object to read data
+        maxElements = scanner.nextInt(); //We read the value provided by the user
+        for (int counter = 0; counter < maxElements; counter++) {
+            System.out.println("counter = " + counter);
+        }
+    }
+}
