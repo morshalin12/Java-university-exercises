80e1792076923bc2d0da7d40913cc77e5a9ffcf0
Kamila de fatima santos oliveira <32311268+Kamilahsantos@users.noreply.github.com>
2018-08-20 15:58:48 -0300

    while exercise

---
+++ WhileLoopTestV2.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Scanner;
+
+public class WhileLoopTestV2 {
+    public static void main(String[] args) {
+        System.out.println("Enter the number of elements to iterate:");
+        int maxElements;
+        Scanner scanner = new Scanner(System.in); //Creation of the Scanner object to read data
+        maxElements = scanner.nextInt(); //We read the value provided by the user
+        int counter = 0;//Initialize the counter
+        while (counter < maxElements) {
+            System.out.println("counter = " + counter);
+            counter++;
+        }
+    }
+}
